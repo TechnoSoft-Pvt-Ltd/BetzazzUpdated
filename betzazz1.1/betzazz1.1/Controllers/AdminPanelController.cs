6b274513b25f3b50e8cb7dc4cd7c6eead92f8549
Mohd-Umar <34959579+Mohd-Umar@users.noreply.github.com>
2018-09-29 16:07:43 +0530

    AdminPalnel Done

---
+++ betzazz1.1/betzazz1.1/Controllers/AdminPanelController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace betzazz1._1.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        // GET: AdminPanel
+        public ActionResult UserControl()
+        {
+            return View();
+        }
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        public ActionResult ManageUser()
+        {
+            return View();
+        }
+        public ActionResult BetControl()
+        {
+            return View();
+        }
+        public ActionResult AddBets()
+        {
+            return View();
+        }
+        public ActionResult ManageBets()
+        {
+            return View();
+        }
+        public ActionResult BalanceControl()
+        {
+            return View();
+
+        }
+        public ActionResult BalanceTransaction()
+        {
+            return View();
+        }
+        public ActionResult UpdateBalance()
+        {
+            return View();
+        }
+        public ActionResult ManageBalance()
+        {
+            return View();
+        }
+
+    }
+}
