d90f57ba450eeda01a175bc68999d39c10ae5a00
Mohd-Umar <34959579+Mohd-Umar@users.noreply.github.com>
2018-10-03 19:31:41 +0530

    ok

---
+++ betzazz1.1/betzazz1.1/Models/LiveEventList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace betzazz1._1.Models
+{
+    public class LiveEventList
+    {
+        public string success { get; set; }
+        public System.Collections.ObjectModel.Collection<Results> results { get; set; }
+    }
+    public class Results
+    {
+        public string id { get; set; }
+        public string sport_id { get; set; }
+        public string time { get; set; }
+        public string time_status { get; set; }
+        public League league { get; set; }
+        public Home home { get; set; }
+        public Away away { get; set; }
+    }
+
+    public class League
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Home
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Away
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class FullNewEL
+    {
+        public string matchId { get; set; }
+        public string time { get; set; }
+        public string timeStatus { get; set; }
+        public string leagueId { get; set; }
+        public string leagueName { get; set; }
+        public string homeTeamId { get; set; }
+        public string homeTeamName { get; set; }
+        public string awayTeamId { get; set; }
+        public string awayTeamName { get; set; }
+        public string sport_id { get; set; }
+    }
+    public class FullNewELFT
+    {
+        public string matchId { get; set; }
+        public string time { get; set; }
+        public string timeStatus { get; set; }
+        public string leagueId { get; set; }
+        public string leagueName { get; set; }
+        public string homeTeamId { get; set; }
+        public string homeTeamName { get; set; }
+        public string awayTeamId { get; set; }
+        public string awayTeamName { get; set; }
+        public string sport_id { get; set; }
+
+    }
+}
