2b8ecba4438d247a2230fab56eabe0ce39ffb40e
Mohd-Umar <34959579+Mohd-Umar@users.noreply.github.com>
2018-10-04 19:56:51 +0530

    04 zoct Commite

--- betzazz1.1/betzazz1.1/BusnessLogics/Global.cs
+++ betzazz1.1/betzazz1.1/BusnessLogics/Global.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using betzazz1._1.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading;
 using System.Web;
+using System.Web.Script.Serialization;
 
 namespace betzazz1._1.BusnessLogics
 {
     public class Global
     {
-      // Configuration of connection string.
-         public static string connectionString=ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        LiveEventList Live = new LiveEventList();
+        // Configuration of connection string.
+        public static string connectionString=ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 
         public List<string> FailedProductIdsList { get; set; }
 
@@ -23,6 +26,15 @@ namespace betzazz1._1.BusnessLogics
 
         public string FotballPMGetJson { get; set; }
 
+
+        public static int crleagueid { get; set; }
+        public  int ftleagueid { get; set; }
+       
+
+
+        public string  LeagueName { get; set; }
+        public string EventId { get; set; }
+        public string EventName { get; set; }
 
 
         // Funtion for Get Inplay Event JSON String from Data Base.
@@ -117,6 +129,90 @@ namespace betzazz1._1.BusnessLogics
                 throw ew;
             }
 
+        }
+        public void InplayCR()
+        {
+            //bool Doprocessing = true;
+            //while (Doprocessing)
+            //{
+                try
+                {
+                   GetIPEventList();
+                    string cripjson = CricketIPGetJson.ToString();
+                    string ftipjson = FotballIPGetJson.ToString();
+                    var serializer = new JavaScriptSerializer();
+
+                    LiveEventList CRLiveList = serializer.Deserialize<LiveEventList>(cripjson);
+                    int matchCount = CRLiveList.results.Count();
+                    if (CRLiveList.success == "1" && CRLiveList.results != null)
+                    {
+                        for (int i = 0; i < matchCount; i++)
+                        {
+                            FullNewEL FulLive = new FullNewEL();
+                            FulLive.matchId = CRLiveList.results[1].id;
+                            FulLive.sport_id = CRLiveList.results[i].sport_id;
+                            FulLive.time = CRLiveList.results[i].time;
+                            FulLive.timeStatus = CRLiveList.results[i].time_status;
+                            FulLive.leagueId = CRLiveList.results[i].league.id;
+                            FulLive.leagueName = CRLiveList.results[i].league.name;
+                            FulLive.homeTeamId = CRLiveList.results[i].home.id;
+                            FulLive.homeTeamName = CRLiveList.results[i].home.name;
+                            FulLive.awayTeamId = CRLiveList.results[i].away.id;
+                            FulLive.awayTeamName = CRLiveList.results[i].away.name;
+                            int LocalLeaguid = Convert.ToInt32(FulLive.leagueId);
+                            crleagueid = Convert.ToInt32(FulLive.leagueId);
+                            if (FulLive.leagueName.Contains("Test") && crleagueid == LocalLeaguid)
+                            {
+                                //test ts = new test();
+                                LeagueName = FulLive.leagueName;
+                                EventId = FulLive.matchId;
+                                EventName = FulLive.homeTeamName + " VS " + FulLive.awayTeamName;
+                                LocalLeaguid = 0;
+                            }
+                            else if((FulLive.leagueName.Contains("T20") && crleagueid == LocalLeaguid))
+                            {
+                            LeagueName = FulLive.leagueName;
+                            EventId = FulLive.matchId;
+                            EventName = FulLive.homeTeamName + " VS " + FulLive.awayTeamName;
+                            LocalLeaguid = 0;
+                            }
+                        }
+                    }
+
+                    //LiveEventList FTLiveList = serializer.Deserialize<LiveEventList>(ftipjson);
+                    //int matchcountFt = FTLiveList.results.Count();
+                    //if (FTLiveList.success == "1" && FTLiveList.results != null)
+                    //{
+                    //    for (int j = 0; j < matchcountFt; j++)
+                    //    {
+                    //        FullNewELFT FTEventList = new FullNewELFT();
+                    //        FTEventList.sport_id = FTLiveList.results[j].sport_id;
+                    //        FTEventList.matchId = FTLiveList.results[j].id;
+                    //        FTEventList.time = FTLiveList.results[j].time;
+                    //        FTEventList.timeStatus = FTLiveList.results[j].time_status;
+                    //        FTEventList.leagueId = FTLiveList.results[j].league.id;
+                    //        FTEventList.leagueName = FTLiveList.results[j].league.name;
+                    //        FTEventList.homeTeamId = FTLiveList.results[j].home.id;
+                    //        FTEventList.homeTeamName = FTLiveList.results[j].home.name;
+                    //        FTEventList.awayTeamId = FTLiveList.results[j].away.id;
+                    //        FTEventList.awayTeamName = FTLiveList.results[j].away.name;
+                    //        int LocalLeaguid = Convert.ToInt32(FTEventList.leagueId);
+                    //        ftleagueid = Convert.ToInt32(FTEventList.leagueId);
+
+                    //        if (FTEventList.leagueName.Contains("Test") && ftleagueid == LocalLeaguid)
+                    //        {
+                    //            LocalLeaguid = 0;
+                    //        }
+
+                    //    }
+                    //}
+                }
+                catch (Exception et)
+                {
+                    throw et;
+                }
+               // Thread.Sleep(1000);
+           // }
         }
 
     }
--- betzazz1.1/betzazz1.1/Models/test.cs
+++ betzazz1.1/betzazz1.1/Models/test.cs
@@ -7,6 +7,20 @@ namespace betzazz1._1.Models
 {
     public class test
     {
-        public List<string> FailedProductIdsList { get; set; }
+        public string LeagueName { get; set; }
+        public string  Eventid { get; set; }
+        public string EventName { get; set; }
+
+    }
+    public class ViewModel
+    {
+        public dynamic list { get; set; }      
+        public IList<string> Links { get; set; }
+    }
+    public class List
+    {
+        public string LeagueName { get; set; }
+        public string Eventid { get; set; }
+        public string EventName { get; set; }
     }
 }
