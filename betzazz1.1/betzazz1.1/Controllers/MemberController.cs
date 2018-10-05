c1dafe228c153d82e278c9d06d812100d207e1cc
Rahul Kumar <41166582+kmr-rahul@users.noreply.github.com>
2018-10-05 19:42:31 +0530

    05-oct

---
+++ betzazz1.1/betzazz1.1/Controllers/MemberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace betzazz1._1.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Member()
+        {
+            return View();
+        }
+        public ActionResult Bank()
+        {
+            return View();
+        }
+        public ActionResult Bets()
+        {
+            return View();
+        }
+       
+    }
+}
