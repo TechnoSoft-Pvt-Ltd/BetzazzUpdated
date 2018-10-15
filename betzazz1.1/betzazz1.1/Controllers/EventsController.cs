af9ae865c4682718db5cf2a5eedff12af7f3e7ea
Shakeb007 <Shakebanwer007@gmail.com>
2018-10-15 19:39:11 +0530

    shakeb

--- betzazz1.1/betzazz1.1/Controllers/EventsController.cs
+++ betzazz1.1/betzazz1.1/Controllers/EventsController.cs
@@ -140,12 +140,8 @@ namespace betzazz1._1.Controllers
            // var deserialized = JsonConvert.DeserializeObject<LiveList>(FottballInplay);
 
             var serilezer = new JavaScriptSerializer();
-<<<<<<< HEAD
-=======
-
            // LiveList LiveFT = serilezer.Deserialize<LiveList>(FottballInplay);
 
->>>>>>> 8cef2516623833f743abaae09673255e012d1fd9
             LiveEventList LiveFT = serilezer.Deserialize<LiveEventList>(FottballInplay);
             
             //int count = LiveFT.results.Count();
@@ -183,10 +179,7 @@ namespace betzazz1._1.Controllers
             //   // Arr1.Add(newList);
             //}
             
-<<<<<<< HEAD
 
-=======
->>>>>>> 8cef2516623833f743abaae09673255e012d1fd9
             ViewBag.LiveFT = LiveFT;
             return View();
 
