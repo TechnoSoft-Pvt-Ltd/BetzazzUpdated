072a1cb403d34a001b6c0214eb67d8c8aa20ed25
Mohd-Umar <34959579+Mohd-Umar@users.noreply.github.com>
2018-10-19 12:37:40 +0530

    Done

--- betzazz1.1/betzazz1.1/Models/test.cs
+++ betzazz1.1/betzazz1.1/Models/test.cs
@@ -23,4 +23,70 @@ namespace betzazz1._1.Models
         public string Eventid { get; set; }
         public string EventName { get; set; }
     }
+    //Class for Betslip
+
+    public  class Betslip
+    {
+
+        public static string EventName = "";/* { get; set; }*/
+        public static string MarketNnme { get; set; }
+        public static string Selection { get; set; }
+        public static string Rate { get; set; }
+    }
+
+
+    public class MatchOdds
+    {
+        public static string EventNAme { get; set; }
+        public static string MarketName { get; set; }
+        public static string MarketTime { get; set; }
+        public static string Team1 { get; set; }
+
+        public static string Odds1 { get; set; }
+
+        public static string Team2 { get; set; }
+        public static string Odds2 { get; set; }
+    }
+
+    // For To win the toss
+    public class TowWintheToss
+    {
+        public static string EventNAme { get; set; }
+        public static string MarketName { get; set; }
+        public static string MarketTime { get; set; }
+        public static string Team1 { get; set; }
+
+        public static string Odds1 { get; set; }
+
+        public static string Team2 { get; set; }
+        public static string Odds2 { get; set; }
+    }
+
+    // For Draw No Bet Market
+    public class DrawNoBet
+    {
+        public static string EventNAme { get; set; }
+        public static string MarketName { get; set; }
+        public static string MarketTime { get; set; }
+        public static string Team1 { get; set; }
+
+        public static string Odds1 { get; set; }
+
+        public static string Team2 { get; set; }
+        public static string Odds2 { get; set; }
+    }
+
+    // For Double Chance
+    public class DBLChance
+    {
+        public static string EventNAme { get; set; }
+        public static string MarketName { get; set; }
+        public static string MarketTime { get; set; }
+        public static string Team1 { get; set; }
+
+        public static string Odds1 { get; set; }
+
+        public static string Team2 { get; set; }
+        public static string Odds2 { get; set; }
+    }
 }
